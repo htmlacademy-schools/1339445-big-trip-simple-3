631c0afeec180b13369684a8220d723145bb9ea2
FAbrickA <schilonosov.alex@yandex.ru>
2023-04-19 21:21:43 +0300

    minor fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,30 @@
-import { resolve } from 'path';
-import CopyPlugin from "copy-webpack-plugin";
+const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
 
-export const entry = './src/main.js';
-export const output = {
-  filename: 'bundle.js',
-  path: resolve(__dirname, 'build'),
-  clean: true,
-};
-export const devtool = 'source-map';
-export const plugins = [
-  new CopyPlugin({
-    patterns: [{ from: 'public' }],
-  }),
-];
-export const module = {
-  rules: [
-    {
-      test: /\.js$/,
-      exclude: /(node_modules)/,
-      use: ['babel-loader']
-    },
-    {
-      test: /\.css$/i,
-      use: ['style-loader', 'css-loader']
-    }
-  ]
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
+  },
+  devtool: 'source-map',
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: 'public' }],
+    }),
+  ],
+  module: {
+    rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: ['babel-loader']
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        }
+    ]
+  }
 };
